7cdc535c91b2c0d697846e69c3e2371f3205526d
Marcus Soares <marcus.silva.soares1@gmail.com>
2026-01-10 16:02:13 -0300

    Adding error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -6,7 +6,7 @@ export const errorHandler: ErrorRequestHandler = async (err, req, res, next)=> {
         res.status(err.status).json({message: err.message})
 
     } else if (err instanceof Error)  {
-        res.status(400  ).json({message: err.message})
+        res.status(400).json({message: err.message})
     }else {
         res.status(500).json({message: err.message})
     }
