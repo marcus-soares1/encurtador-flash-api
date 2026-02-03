504125a70fd140f0ad4c602bca841fdc976b7beb
Marcus Soares <marcus.silva.soares1@gmail.com>
2026-02-03 20:51:12 -0300

    Adding zod error treatment

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,28 @@
 import { ErrorRequestHandler } from "express";
 import { HttpError } from "../errors/HttpError";
+import { ZodError } from "zod";
+import { Prisma } from "@prisma/client";
 
 export const errorHandler: ErrorRequestHandler = async (err, req, res, next)=> {
-    if (err instanceof HttpError) {
+    if (err instanceof ZodError) {
+        const fields: Record<string, string> = {}
+
+        err.errors.forEach(error => {
+        const field = error.path.join('.') || 'body'
+            fields[field] = error.message
+        })
+
+        return res.status(400).json({
+        message: 'Dados inválidos',
+            fields
+        })
+    } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2025') {
+            return res.status(404).json({
+                message: 'Registro não encontrado'
+            })
+        }
+    } else if (err instanceof HttpError) {
         res.status(err.status).json({message: err.message})
 
     } else if (err instanceof Error)  {
